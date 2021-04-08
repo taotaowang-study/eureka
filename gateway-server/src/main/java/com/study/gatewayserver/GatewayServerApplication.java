2500c8ab2d4c7fa20883a13671f9ebd48cece376
taotaowang-study <864734049@qq.com>
2021-04-08 18:40:21 +0800

    网关 熔断

--- gateway-server/src/main/java/com/study/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/study/gatewayserver/GatewayServerApplication.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class GatewayServerApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
     }
