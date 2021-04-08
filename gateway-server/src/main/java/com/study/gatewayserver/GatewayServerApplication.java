5be5e292978c6ad1498c2ededc927ae951b42cee
taotaowang-study <864734049@qq.com>
2021-04-08 18:53:09 +0800

    测试

--- gateway-server/src/main/java/com/study/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/study/gatewayserver/GatewayServerApplication.java
@@ -13,9 +13,6 @@ import org.springframework.context.annotation.Bean;
 public class GatewayServerApplication {
 
 
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
     }
